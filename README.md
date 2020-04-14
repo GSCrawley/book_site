85541d6a69cfc5501590ac29f79d0b703c2f0f6e
GSCrawley <gscrawley@gmail.com>
2020-04-14 11:01:01 -0700

    part 1 done

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # book_site
+How would we filter for all books with titles containing the word ‘Django’?
+A: book_list = Book.objects.filter(book__title = 'Django')
+
+
+How would we filter for all books with tag ‘Fiction’?
+A: book_list = Book.objects.filter(book__tag ='Fiction')
+
+
+How would we filter for all authors who have written books containing the word ‘Django’? HINT: We can use the book field to refer to an author’s books, even though the Author model doesn’t explicitly contain it!
+
+A: author_list = Author.objects.filter(book__title = 'Django')
--- books/models.py
+++ books/models.py
@@ -1,10 +1,26 @@
 from django.db import models
 
 # Create your models here.
+class Tag(models.Model):
+    name = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.namee
+
+class Author(models.Model):
+    name = models.CharField(max_length=50)
+    birth_date = models.DateField()
+
+    def __str__(self):
+        return self.name
+
 class Book(models.Model):
     title = models.CharField(max_length=50)
-    num_pages = models.IntegerField(max_length=100)
-    date_published = models.DateField(blank=True, Null=True)
+    num_pages = models.IntegerField()
+    date_published = models.DateField(blank=True, null=True)
+    author = models.ForeignKey(Author, on_delete=models.CASCADE, null=True)
+    tags = models.ManyToManyField(Tag)
 
     def __str__(self):
         return self.title
+
