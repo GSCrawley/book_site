5fe31e61441b78087ed2e6e5955f22f80e503188
GSCrawley <gscrawley@gmail.com>
2020-04-14 17:03:28 -0700

    everything works except tags

--- books/models.py
+++ books/models.py
@@ -5,7 +5,7 @@ class Tag(models.Model):
     name = models.CharField(max_length=50)
 
     def __str__(self):
-        return self.namee
+        return self.name 
 
 class Author(models.Model):
     name = models.CharField(max_length=50)
